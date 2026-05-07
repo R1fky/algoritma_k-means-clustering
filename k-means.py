f9533751d8157a45902cdfad964416e7b1fa1636
R1fky <rifkiyuda11@gmail.com>
2026-05-07 17:19:07 +0700

    memperbaiki code k-means

--- k-means.py
+++ k-means.py
@@ -4,71 +4,85 @@
 # aturan k- means
 # jika jarak ke C1 < jarak ke C2 → masuk Cluster 1
 # jika jarak ke C2 < jarak ke C1 → masuk Cluster 2
-def k_means_1_iterasi(datas, c1, c2):
+
+#fungsi menghitung jarak
+def distance(p, c):
+    return ((p[0] - c[0])**2 + (p[1] - c[1])**2) ** 0.5
+
+#fungsi menghitung new centroid
+def calculate_new_centroid(klusters) :
+    kluster = klusters.copy()
+    
+    if len(klusters) == 0:
+        return (0,0)
+    # for x axis
+    sum_x = 0
+    sum_y = 0
+    data_x = []
+    data_y = []
+    for k in kluster:
+        sum_x += k[0]
+        sum_y += k[1]
+        data_x.append(k[0])
+        data_y.append(k[1])
+    new_x = sum_x / len(data_x)
+    new_y = sum_y / len(data_y)
+    
+    return new_x, new_y
+
+def k_means(datas, c1, c2):
     data = datas.copy()
-    cluster1_lokal = []
-    cluster2_lokal = []
-    #penjumlahan 
-    for d in data:
-        hasil_c1 = ((d[0] - c1[0])**2 + (d[1] - c1[1])**2) ** 0.5
-        hasil_c2 = ((d[0] - c2[0])**2 + (d[1] - c2[1])**2) ** 0.5 
-        print(f"jarak c1 : {hasil_c1}")
-        print(f"jarak c2 : {hasil_c2}")
+    
+    # 1. reset cluster kosong
+    # 2. assign cluster
+    # 3. hitung centroid baru
+    # 4. cek apakah centroid berubah
+    # 5. update centroid
+    
+    perubahan = True
+    iterasi = 0
+    while(perubahan):
+        iterasi += 1 
+        cluster1 = []
+        cluster2 = []
+        # hitung jarak dan assign cluster dengan centroid baru
+        for d in data:
+            jarak_c1 = distance(d, c1)
+            jarak_c2 = distance(d, c2)
+            
+            if(jarak_c1 < jarak_c2):
+                cluster1.append(d)
+            else:
+                cluster2.append(d)
+        print('iterasi', iterasi)
+        print('clutser 1 : ',cluster1)
+        print('cluster 2 : ',cluster2)
         
-        if(hasil_c1 < hasil_c2):
-            cluster1_lokal.append(d)
-        elif(hasil_c2 < hasil_c1):
-            cluster2_lokal.append(d)
-            
-            
-    return cluster1_lokal, cluster2_lokal
+        #hitung ulang centroid baru
+        centroid1_baru = calculate_new_centroid(cluster1)
+        centroid2_baru = calculate_new_centroid(cluster2)
+        
+        print("centroid lama:", c1, c2)
+        print("centroid baru:", centroid1_baru, centroid2_baru)
+        
+        if(centroid1_baru == c1 and centroid2_baru == c2):
+            perubahan = False
+        else:
+            c1 = centroid1_baru
+            c2 = centroid2_baru
+    
+    return cluster1, cluster2
         
     
 list_data = [(41,19), (47,100), (33,57), (29,19)]
-print(len(list_data[0]))
-c1 = (41,19)
-c2 = (47,100)
+# c1 = (41,19)
+# c2 = (47,100)
 
-kluster_1, kluster_2 = k_means_1_iterasi(list_data, c1, c2)
+c1 = (100,0)
+c2 = (0,100)
+
+kluster_1, kluster_2 = k_means(list_data, c1, c2)
 
 print('kluster 1 :', kluster_1)
 print('kluster 2 :', kluster_2)
-
-#menentukan nilai centroid baru
-# centorid 1
-jumlahIndex0 = 0
-jumlahIndex1 = 0
-panjangIndex0 = []
-panjangIndex1 = []
-
-for i in kluster_1:
-    jumlahIndex0 += i[0]
-    jumlahIndex1 += i[1]
-    panjangIndex0.append(i[0])
-    panjangIndex1.append(i[1])
-    
-new_centroid1 = ((jumlahIndex0 / len(panjangIndex0)), (jumlahIndex1 / len(panjangIndex1)))
-print(f"nilai new centroid 1 : {new_centroid1}") 
-
-# cenrtroid 2
-jumlahIndex0_c2 = 0
-jumlahIndex1_c2 = 0
-panjangIndex0_c2 = []
-panjangIndex1_c2 = []
-for i in kluster_2:
-    jumlahIndex0_c2 += i[0]
-    jumlahIndex1_c2 += i[1]
-    panjangIndex0_c2.append(i[0])
-    panjangIndex1_c2.append(i[1])
-    
-new_centroid2 = ((jumlahIndex0_c2 / len(panjangIndex0_c2)), (jumlahIndex1_c2 / len(panjangIndex1_c2)))
-print(f"nilai new centroid 2 : {new_centroid2}")
-
-#cara clean hitung new centroid
-# sum_x = sum([p[0] for p in kluster_1])
-# sum_y = sum([p[1] for p in kluster_1])
-
-# centroid1 = (sum_x / len(kluster_1), sum_y / len(kluster_1))
-# print(centroid1)
-
-
+        
