af476eeb3aab1459d783084429fd12ffceb396b7
R1fky <rifkiyuda11@gmail.com>
2026-05-16 21:29:56 +0700

    membuat k-means lebih dinamis

--- k-means.py
+++ k-means.py
@@ -1,40 +1,57 @@
 import numpy as np
 
+#fungsi menghitung distance
+def distance(p, c):
+    return ((p[0] - c[0])**2 + (p[1] - c[1])**2) ** 0.5
+
+
+# fungsi kmeans
+def k_means(point_datas, centroids):
+    data = point_datas.copy()
+    centroid = centroids.copy()
+
+        
+    # menghitung distance 
+    status = True
+    while(status):
+        #membuat kluster tergantung centroid
+        K = len(centroid)
+        klusters = []
+        for i in range(K):
+            klusters.append([])
+            
+        for d in data:
+            all_distance = []  
+            for c in range(len(centroid)):
+                pusat = centroid[c]
+                
+                #hitung jarak/ distance
+                distances = distance(d, pusat)
+                all_distance.append((distances))
+                
+            # mencari index terdekat
+            index_terdekat = np.argmin(all_distance) 
+                
+            #masuk ke cluster 
+            klusters[index_terdekat].append(d) 
+        
+        for k in range(len(klusters)):
+            print(f"kluser {k + 1} : {klusters[k]}")
+            
+        status = False
+        
+    
+
+# data yang mau di cluster 
 x = np.array([1,2,3,8,9,10,50,55,60])
 y = np.array([1,2,3,8,9,10,50,55,60])
+datas = np.column_stack((x, y))
 
-data_points = np.column_stack((x, y))
-
-centroids = np.array([
+#centroid
+c = np.array([
     [1,2],
     [8,9],
     [50,50]
 ])
 
-# membuat cluster
-K = len(centroids)
-
-klusters = []
-
-for i in range(K):
-    klusters.append([])
-    
-        
-for data in data_points:
-    all_distance_centroid = []
-    
-    for i in range(len(centroids)):
-        centroid = centroids[i]
-        
-        #menghitung jarak equaliden
-        distance = ((data[0] - centroid[0])**2 + (data[1] - centroid[1])**2)** 0.5
-        
-        all_distance_centroid.append(distance)
-        
-    index_terdekat = np.argmin(all_distance_centroid)
-    
-    klusters[index_terdekat].append(data)
-
-for i in range(len(klusters)):
-    print(f"kluster {i + 1} : {klusters[i]}")
-    
+k_means(datas, c)
